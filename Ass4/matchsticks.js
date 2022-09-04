286456069fcf080a964f00477dc93ebaf139c5ed
Josue Isamuna <josamuna2009@gmail.com>
2022-09-04 15:24:58 +0200

    Complete assignment4

---
+++ Ass4/matchsticks.js
@@ -0,0 +1,28 @@
+/*
+    Author: Josue Isamuna Nkembo
+    Email : josamuna2009@gmail.com
+
+    Create a function that takes a number (step) as an argument and returns the number of matchsticks 
+    in that step. See step 1, 2 and 3 in the image above. Take input from the user in the function 
+    parameter and return the output using the return statement.
+    examples:
+    matchHouses(1) ➞ 6
+    matchHouses(4) ➞ 21
+    matchHouses(87) ➞ 436
+*/
+
+function matchHouse(step) {
+    let init = 6;
+    return init += ((step - 1) * 5);
+}
+
+var matchsticks = matchHouse(1);
+console.log(matchsticks);
+var matchsticks = matchHouse(2);
+console.log(matchsticks);
+var matchsticks = matchHouse(3);
+console.log(matchsticks);
+var matchsticks = matchHouse(4);
+console.log(matchsticks);
+var matchsticks = matchHouse(87);
+console.log(matchsticks);
