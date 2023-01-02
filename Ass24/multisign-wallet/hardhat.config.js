e2c5a7bfa700895aa79fa86780154dd557284ce2
Josue Isamuna <josamuna2009@gmail.com>
2023-01-03 01:36:35 +0200

    complete some test cases with gas report generation

--- Ass24/multisign-wallet/hardhat.config.js
+++ Ass24/multisign-wallet/hardhat.config.js
@@ -1,16 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("hardhat-gas-reporter");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.6",
-
-  gasReporter: {
-    enabled: true,
-    currency: "USD",
-    noColors: true,
-    outputFile: "gasReportFile.txt",
-    coinmarketcap: "807ae37f-4bab-4572-b60c-a2baae6e5a74",
-    // token: "matic",
-  },
 };
